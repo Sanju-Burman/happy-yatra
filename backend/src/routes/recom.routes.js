ca4dec2ea4070d8ca78923c97cf7b2ca8adcf824
Sanju Burman <sanju.burman@reak.in>
2026-04-16 19:03:19 +0530

    refactor(destinations): remove dead recom controller

--- backend/src/routes/recom.routes.js
+++ backend/src/routes/recom.routes.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const router = express.Router();
-// const { reccom } = require("../controllers/recom.controller");
 const { getDestinations, getDestinationById } = require("../controllers/destinations.controller");
 
-// router.post("/", reccom);
+
 router.get("/", getDestinations);
 router.get("/:id", getDestinationById);
 module.exports = router;
